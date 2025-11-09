5aabc18c69d55cae733be1738b1ff42e6a41108b
Artem Shush <shushtain@gmail.com>
2025-11-09 17:35:17 +0200

    feat(yazi): gvfs

--- yazi.sh
+++ yazi.sh
@@ -13,6 +13,12 @@ packages=(
     yazi
     zoxide
     ueberzugpp
+    gvfs
+    glib2
+    gvfs-mtp
+    gvfs-afc
+    gvfs-google
+    gvfs-gphoto2
 )
 
 yay -S --needed "${packages[@]}"
@@ -23,5 +29,5 @@ ya pkg add yazi-rs/plugins:git
 ya pkg add yazi-rs/plugins:mount
 ya pkg add yazi-rs/plugins:toggle-pane
 ya pkg add yazi-rs/plugins:vcs-files
-
+ya pkg add boydaihungst/gvfs
 ya pkg add KKV9/compress
